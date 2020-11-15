66c7d47d774427a3307b529209dbffecda543116
Hunter Wallace <32723509+Hjwallace@users.noreply.github.com>
2020-11-15 17:34:52 -0500

    Beginning Development
    Submitting Code to a seprate branch to track progress and wait until a first iteration feature is met

---
+++ 24x7System-Monitoring/SystemMonitoring.Backend/Handlers/ApiCallHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace SystemMonitoring.Backend.Handlers
+{
+    class ApiCallHandler
+    {
+        public static async System.Threading.Tasks.Task<string> ApiCallAsync()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US"),
+                Headers =
+    {
+        { "x-rapidapi-key", "d69aa8f42bmsh02802141d683248p166b67jsn04426870fe95" },
+        { "x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return body;
+            }
+        }
+    }
+}
