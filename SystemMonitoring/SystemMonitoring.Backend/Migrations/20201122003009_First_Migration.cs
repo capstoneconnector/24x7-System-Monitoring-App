b932e3b51500651f0a0dbc43f5cba2b02ad18850
Hunter Wallace <32723509+Hjwallace@users.noreply.github.com>
2020-11-22 15:40:43 -0500

    First Iteration Code Base from Bitbucket

---
+++ SystemMonitoring/SystemMonitoring.Backend/Migrations/20201122003009_First_Migration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SystemMonitoring.Backend.Migrations
+{
+    public partial class First_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestForHangfire",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Job = table.Column<string>(type: "text", nullable: true),
+                    RunTime = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestForHangfire", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestForHangfire");
+        }
+    }
+}
